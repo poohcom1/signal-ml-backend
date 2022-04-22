bf3c83760fd3d1af892fb741629aa79b46b53d32
poohcom1 <pooh677@gmail.com>
2022-04-22 11:32:59 +0700

    Added types and endpoint improvements

--- app.ts
+++ app.ts
@@ -3,11 +3,11 @@ import fs from "fs";
 import path from "path";
 import multer from "multer";
 import { setUp } from "./lib/setup";
-import { convert, testConvert } from "./lib/converter/converter";
-import cors from 'cors'
+import { convert, parse_configs } from "./lib/converter/converter";
+import cors from "cors";
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT;
 
 const FILES_DIR_NAME = "files";
 const MODELS_DIR_NAME = "models";
@@ -19,35 +19,80 @@ const files_dir = path.join(__dirname, FILES_DIR_NAME);
 
 const upload = multer({ dest: files_dir });
 
-app.use(cors())
+app.use(cors());
 
-app.post("/convert/", upload.single("midi"), (req, res) => {
-  console.log("[Convert] received midi");
+app.get("/models", (req, res) => {
+  const models = fs.readdirSync(model_dir);
+
+  const configs = {};
+
+  models.forEach((model) => {
+    configs[model] = parse_configs(path.join(MODELS_DIR_NAME, model), true);
+  });
+
+  res.send(configs);
+});
+
+app.post("/convert/:model", upload.single("midi"), (req, res) => {
+  const modelName = req.params.model;
+
+  console.log(`[Convert] [${modelName}] Received midi `);
+
+  const options = JSON.parse(req.body.options);
+  const bpm = req.body.bpm;
 
   let timerStart = process.hrtime();
 
-  const newDirPath = req.file.path + "_project"
-  fs.mkdirSync(newDirPath)
-  const newFilepath = path.join(newDirPath, req.file.filename)
-  fs.renameSync(req.file.path, newFilepath)
+  console.log(req.file.path);
 
-  const output_path = path.join(newDirPath, req.file.filename + ".wav");
+  // Setup files
+  const newDirPath = req.file.path + "_project";
+  fs.mkdirSync(newDirPath);
+  const newFilepath = path.join(newDirPath, req.file.filename);
+  fs.renameSync(req.file.path, newFilepath);
+  const file_name = path.join(newDirPath, req.file.filename);
+  const output_path = file_name + ".wav";
 
-  let convert_process = convert(req.file.filename, newDirPath, path.join(MODELS_DIR_NAME, "midi2params"))
+  // Convert
+  let convert_process = convert(
+    req.file.filename,
+    newDirPath,
+    path.join(MODELS_DIR_NAME, modelName),
+    options,
+    { bpm }
+  );
 
   convert_process.on("exit", () => {
-    console.log(`[Convert] Finished in ${process.hrtime(timerStart)} ms`);
+    console.info(`[Convert] Finished in ${process.hrtime(timerStart)} ms`);
+
+    if (fs.existsSync(`${file_name}_trimmed.wav`)) {
+      console.log("Audio trimmed!");
+
+      fs.renameSync(`${file_name}_trimmed.wav`, output_path);
+    }
 
     res.sendFile(output_path);
   });
 
   res.on("finish", () => {
-    fs.rmSync(req.file.path + "_project", { recursive: true, force: true });
+    if (process.env.DEBUG) {
+      fs.rmSync(req.file.path + "_project", { recursive: true, force: true });
+    }
   });
-  res.on("error", e => {
-    console.log(e)
-    fs.rmSync(req.file.path + "_project", { recursive: true, force: true });
-  })
+  res.on("error", (e) => {
+    if (process.env.DEBUG) {
+      fs.rmSync(req.file.path + "_project", { recursive: true, force: true });
+    }
+  });
 });
 
-app.listen(port, () => { });
+app.listen(port, () => {
+  // Check configs
+  const models = fs.readdirSync(model_dir);
+
+  models.forEach((model) => {
+    parse_configs(path.join(MODELS_DIR_NAME, model), true);
+  });
+
+  console.info(`[Info] Loaded ${models.length} models.`);
+});
--- lib/converter/converter.ts
+++ lib/converter/converter.ts
@@ -1,78 +1,160 @@
-import fs from 'fs'
-import path from 'path'
-import YAML from 'yaml'
-import { ChildProcess, spawn, exec } from "child_process";
+import fs from "fs";
+import path from "path";
+import YAML from "yaml";
+import { ChildProcess, exec } from "child_process";
 
-const MODEL_MANIFEST_NAME = "manifest.sml.yaml"
-
-export function parse_configs(modelDir: string): Configs {
-  const FILE_DIR = path.join(modelDir, MODEL_MANIFEST_NAME)
-
-  const configData = fs.readFileSync(FILE_DIR, 'utf-8')
-
-  return YAML.parse(configData)
-}
-
-export function convert(filename: string, fileDir: string, modelDir: string): ChildProcess {
-  const configs = parse_configs(modelDir)
-
-  inject_parameters(configs.script, configs.parameters, filename, fileDir)
-
-  return exec(`cd ${modelDir} & ` + configs.script.join(" & "),
-    (err, stdout, stderr) => {
-      if (err) {
-        console.error(`exec error: ${err}`)
-        return
-      }
-
-      console.log(`stdout: ${stdout}`)
-      if (stderr) console.error(`stderr: ${stderr}`)
-    })
-}
-
-export function testConvert(file: Express.Multer.File, path: string) {
-  return exec("ls");
-}
+const MODEL_MANIFEST_NAME = "manifest.sml.yaml";
+const FILE_NAME_PARAM = "BASENAME";
+const DIR_PARAM = "DIR";
+const TEMPO = "TEMPO"
 
 interface Configs {
-  name: string
-  format: "midi" | "musicxml"
-  parameters: Record<string, Parameter>
-  script: Array<string>
+  name: string;
+  format: "midi" | "musicxml";
+  parameters: Record<string, Parameter>;
+  script: Array<string>;
+
+  trimStart?: number;
+  trimEnd?: number;
 }
 
 interface Parameter {
-  type: "number" | "integer" | "string"
-  default: number | string
-  min: number | null
-  max: number | null
+  type: "float" | "int" | "string" | "boolean" | "enum";
+  default: number | string | boolean;
+  min: number | undefined;
+  max: number | undefined;
+  private: boolean | undefined;
+  enum: string[];
 }
 
-const FILE_NAME_PARAM = "BASENAME"
-const DIR_PARAM = "DIR"
+export function parse_configs(
+  modelDir: string,
+  removePrivate = false
+): Configs {
+  const FILE_DIR = path.join(modelDir, MODEL_MANIFEST_NAME);
 
-function inject_parameters(commands: Array<string>, parameters: Record<string, Parameter>, fileName: string, fileDir: string,) {
-  const regex = /\$[a-zA-Z0-9]*/g
+  const configData = fs.readFileSync(FILE_DIR, "utf-8");
+
+  const configs: Configs = YAML.parse(configData);
+
+  for (const key of Object.keys(configs.parameters)) {
+    // Create defaults
+    const param = configs.parameters[key];
+
+    if (typeof param === "string") {
+      const value = param;
+      configs.parameters[key].type = "string";
+      configs.parameters[key].default = value;
+    }
+
+    if (!param.type) {
+      param.type === "string";
+
+      console.warn(
+        `[Config Warning] ${configs.name}: No type specified for "${key}"`
+      );
+    }
+
+    if (param.default === undefined) {
+      switch (param.type) {
+        case "string":
+          param.default = "";
+          break;
+        case "boolean":
+          param.default = false;
+          break;
+        case "int":
+          param.default = 0;
+          break;
+        case "float":
+          param.default = 1.0;
+          break;
+        case "enum":
+          param.default = param.enum[0];
+          break;
+      }
+      console.warn(
+        `[Config Warning] ${configs.name}: No default value specified for "${key}"`
+      );
+    }
+
+    // Remove private configs
+    if (removePrivate && configs.parameters[key].private) {
+      delete configs.parameters[key];
+    }
+  }
+
+  return configs;
+}
+
+export function convert(
+  filename: string,
+  fileDir: string,
+  modelDir: string,
+  options: Record<string, string | number | boolean>,
+  data: { bpm: number }
+): ChildProcess {
+  const configs = parse_configs(modelDir);
+
+  inject_parameters(configs.script, options, filename, fileDir, data.bpm);
+
+  let trimCommand = "";
+
+  // if (configs.trimStart) {
+  //   const file = path.join(fileDir, filename);
+
+  //   trimCommand = ` & ffmpeg -loglevel error -ss ${
+  //     (60 / data.bpm) * configs.trimStart
+  //   } -i ${file}.wav ${file}_trimmed.wav`;
+  // }
+
+  return exec(
+    `cd ${modelDir} & ` + configs.script.join(" & ") + trimCommand,
+    (err, stdout, stderr) => {
+      if (err) {
+        console.error(`[exec error] ${err}`);
+        return;
+      }
+
+      console.log(`[stdout] ${stdout}`);
+      if (stderr) console.error(`[stderr] ${stderr}`);
+    }
+  );
+}
+
+function inject_parameters(
+  commands: Array<string>,
+  parameters: Record<string, string | boolean | number>,
+  fileName: string,
+  fileDir: string,
+  tempo: number,
+) {
+  const regex = /\$[a-zA-Z0-9]*/g;
 
   for (let i = 0; i < commands.length; i++) {
-    let found = commands[i].match(regex)
+    let found = commands[i].match(regex);
 
     if (found) {
       for (let j = 0; j < found.length; j++) {
-        let replace = ""
+        let replace = "";
 
-        switch (found[j].substr(1)) {
+        const key = found[j].substring(1);
+
+        switch (key) {
           case DIR_PARAM:
-            replace = fileDir
-            break
+            replace = fileDir;
+            break;
           case FILE_NAME_PARAM:
-            replace = fileName
-            break
+            replace = fileName;
+            break;
+          case TEMPO:
+            replace = tempo + ""
+            break;
           default:
-            replace = `${parameters[found[j].substr(1)].default}`
+            replace = `${parameters[key]}`;
         }
 
-        commands[i] = commands[i].replace(found[j], replace)
+        commands[i] = commands[i].replace(found[j], replace);
       }
     }
   }
